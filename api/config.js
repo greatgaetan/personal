30339f09a58bcc48b0f1664408d5b5bce0f690e6
Gaëtan Didderen <g.didderen@student.helmo.be>
2022-05-04 12:49:11 +0200

    add api

---
+++ api/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  RAPIDAPI_KEY: "7e6f85c44cmsha3754419e95241ap1d92cbjsnb8ae8e8a225c",
+  OPENWEATHERAPI_KEY: "d56bd010e37d5a99462eb6bfd82c4324",
+};
+
+export default CONFIG;
---
+++ api/menu.js
@@ -0,0 +1,44 @@
+const MENU = {
+  sandwiches: [
+    {
+      "poulet croquant": {
+        name: "Poulet croquant",
+        ingredients: ["poulet", "cornichons", "oignons", "salade"],
+        price: 5.5,
+      },
+      dagobert: {
+        name: "Dagobert",
+        ingredients: ["jambon", "fromage", "cornichons", "tomates", "salade"],
+        price: 4.5,
+      },
+      "jambon beurre": {
+        name: "Jambon beurre",
+        ingredients: ["jambon", "beurre", "cornichons", "oignons", "salade"],
+        price: 4.5,
+      },
+      salami: {
+        name: "Salami",
+        ingredients: ["salami", "beurre", "cornichons", "capres", "salade"],
+        price: 5,
+      },
+      "el diablo": {
+        name: "El Diablo",
+        ingredients: [
+          "steak haché",
+          "sauce chili",
+          "piment",
+          "fromage",
+          "salade",
+        ],
+        price: 6.5,
+      },
+      "le petit belge": {
+        name: "Le petit Belge",
+        ingredients: ["steak hacké", "fromage", "frites", "oignons frits"],
+        price: 6.5,
+      },
+    },
+  ],
+};
+
+export default MENU;
